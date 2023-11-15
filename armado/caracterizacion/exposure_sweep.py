4cc94cfde04e960b42712e9a871db81a3b85d680
Joaquin Torres <torres@tandar.cnea.gov.ar>
2023-11-15 18:53:40 -0300

    Add example for using the microscope.

---
+++ armado/caracterizacion/exposure_sweep.py
@@ -0,0 +1,23 @@
+import numpy as np
+from chanoscope.chanoscope.drivers.camera import CameraQHY
+
+CAMERA_ID = 0
+RGB_MATRIX_PORT = "/dev/ttyACM0"
+
+qhy = CameraQHY(bit_depth=14)
+qhy.open_resource(CAMERA_ID)
+exposure_range = np.arange(200, 1000, 50)  # TODO
+gain = 100
+
+def exposure_sweep(exp_range, gain, qhy_camera):
+    raw_images = []
+    for exposure in exp_range:
+        qhy_camera.set_gain_exposure(gain, exposure)
+        img = qhy_camera.single_get_frame()
+        raw_images.append(img)
+    return raw_images
+
+raw_images = exposure_sweep(exposure_range, gain, qhy)
+
+for image in raw_images:
+    np.save("exposure_sweep_gain_{}_exposure_{}".format(gain, exposure), image)
